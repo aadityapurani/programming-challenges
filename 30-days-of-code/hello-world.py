bfd083198c7409c24de7e53c4a5baed7472d6903
Aaditya Purani <aaditya.purani@gmail.com>
2016-06-28 12:42:01 +0530

    Create hello-world.py

---
+++ 30-days-of-code/hello-world.py
@@ -0,0 +1,7 @@
+#Day 0 Hello World
+#Coded by : Aaditya Purani
+inputString = raw_input() # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+print 'Hello, World.'
+print inputString
