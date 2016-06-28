baffb02475b22dc1a3ddd11aeb5e9df88ed9cccb
Aaditya Purani <aaditya.purani@gmail.com>
2016-06-28 12:43:25 +0530

    Create data-types.py

---
+++ 30-days-of-code/data-types.py
@@ -0,0 +1,10 @@
+#Day 1 HackerRank 30 days of code
+#Coded by Aaditya Purani
+# Declare second integer, double, and String variables.
+x = int(raw_input())
+y = float(raw_input())
+z = raw_input()
+
+print i + x
+print d + y
+print s + z
