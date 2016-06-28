0edd2f0258bd4107631da5cab3a7f8c37b970e66
Aaditya Purani <aaditya.purani@gmail.com>
2016-06-28 12:49:30 +0530

    Create class.py

---
+++ 30-days-of-code/class.py
@@ -0,0 +1,22 @@
+#Day 4 Class vs Instance HackerRank
+#Coded by Aaditya Purani
+class Person:
+
+    def __init__(self, initialAge):
+        if initialAge < 0:
+            print "Age is not valid, setting age to 0."
+            age = 0
+        else:
+            age = initialAge
+        self.age = age
+
+    def amIOld(self):
+        if self.age < 13:
+            print "You are young."
+        elif self.age < 18:
+            print "You are a teenager."
+        else:
+            print "You are old."
+
+    def yearPasses(self):
+        self.age += 1
